937b4833536238520e8298241bac6327de315c52
Yash Ghugardare <yghugardare6@gmail.com>
2023-12-28 19:40:21 +0530

    REDUX setup, tailwind setup and basic Layout Added

---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import authSlice from "./authSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer : {
+       auth : authSlice, 
+    }
+})
+export default store;
